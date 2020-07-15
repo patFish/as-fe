f9179ace1701e20a9e2426c2eb8c05da9b242c46
patFish <fischer.pa7rick@gmail.com>
2020-07-15 13:15:17 +0200

    basic styling

--- src/components/FleetOverview.vue
+++ src/components/FleetOverview.vue
@@ -1,30 +1,57 @@
 <template>
-  <div>
-    <h1>Hello FleetOverview</h1>
-    <Map :locations="locations" />
+  <div class="fleetoverview">
+    <h1 class="header">Flottenübersicht</h1>
+    <Map class="map" :locations="locs" />
+    <FleetTable class="list" :cars="fleet" />
   </div>
 </template>
 
 <script>
-import Map from "./Map";
 import { latLng } from "leaflet";
+import Map from "./Map";
+import FleetTable from "./FleetTable";
 
 export default {
   name: "FleetOverview",
   components: {
-    Map
+    Map,
+    FleetTable
   },
-  data() {
-    return {
-      locations: [
-        latLng(47.41322, -1.219482),
-        latLng(47.41422, -1.250482),
-        latLng(47.41422, -1.250682),
-        latLng(47.41443, -1.251482)
-      ]
-    };
+  props: { fleet: Array },
+
+  computed: {
+    locs() {
+      return this.fleet.map(item => {
+        return latLng(item.Position.lat, item.Position.lon);
+      });
+    }
   }
 };
 </script>
 
-<style></style>
+<style>
+.fleetoverview {
+  display: block;
+}
+.map {
+  grid-area: map;
+}
+.list {
+  grid-area: list;
+}
+.header {
+  grid-area: header;
+}
+
+@media only screen and (min-width: 700px) {
+  .fleetoverview {
+    margin: 5%;
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    grid-template-rows: auto;
+    grid-template-areas:
+      "header header"
+      "map list";
+  }
+}
+</style>
