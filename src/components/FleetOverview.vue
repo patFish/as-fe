65e29d09c009c121cdeef9d56f70ccfba34dee36
patFish <fischer.pa7rick@gmail.com>
2020-07-15 07:45:07 +0200

    fleetvoerview

---
+++ src/components/FleetOverview.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Hello FleetOverview</h1>
+    <Map :locations="locations" />
+  </div>
+</template>
+
+<script>
+import Map from "./Map";
+import { latLng } from "leaflet";
+
+export default {
+  name: "FleetOverview",
+  components: {
+    Map
+  },
+  data() {
+    return {
+      locations: [
+        latLng(47.41322, -1.219482),
+        latLng(47.41422, -1.250482),
+        latLng(47.41422, -1.250682),
+        latLng(47.41443, -1.251482)
+      ]
+    };
+  }
+};
+</script>
+
+<style></style>
