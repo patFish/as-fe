aa07a27c46e4758e0e72e2b5face54c74904e6f9
patFish <fischer.pa7rick@gmail.com>
2020-07-15 13:15:00 +0200

    dummy data

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -27,34 +23,103 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
+      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <!-- <HelloWorld/> -->
+      <FleetOverview :fleet="fleet" />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld';
+import FleetOverview from "./components/FleetOverview";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    // HelloWorld,
+    FleetOverview
   },
-
-  data: () => ({
-    //
-  }),
+  data: () => {
+    return {
+      fleet: [
+        {
+          id: 12345678,
+          name: "Executive car 1",
+          vin: "ASD423E3D3RF5",
+          make: "Mazda",
+          model: "CX-5",
+          year: "2019",
+          fuelType: "petrol",
+          type: "SUV",
+          Position: {
+            lat: 3.995,
+            lon: 43.2221
+          },
+          odometer: 43546,
+          fuel: 33.4,
+          battery: 12.7
+        },
+        {
+          id: 12345678,
+          name: "Executive car 1",
+          vin: "ASD423E3D3RF5",
+          make: "Mazda",
+          model: "CX-5",
+          year: "2019",
+          fuelType: "petrol",
+          type: "SUV",
+          Position: {
+            lat: 3.795,
+            lon: 43.1221
+          },
+          odometer: 43546,
+          fuel: 33.4,
+          battery: 12.7
+        },
+        {
+          id: 12345678,
+          name: "Executive car 1",
+          vin: "ASD423E3D3RF5",
+          make: "Mazda",
+          model: "CX-5",
+          year: "2019",
+          fuelType: "petrol",
+          type: "SUV",
+          Position: {
+            lat: 4.095,
+            lon: 43.2221
+          },
+          odometer: 43546,
+          fuel: 33.4,
+          battery: 12.7
+        },
+        {
+          id: 12345678,
+          name: "Executive car 1",
+          vin: "ASD423E3D3RF5",
+          make: "Mazda",
+          model: "CX-5",
+          year: "2019",
+          fuelType: "petrol",
+          type: "SUV",
+          Position: {
+            lat: 4.195,
+            lon: 43.2221
+          },
+          odometer: 43546,
+          fuel: 33.4,
+          battery: 12.7
+        }
+      ]
+    };
+  }
 };
 </script>
