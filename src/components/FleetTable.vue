f5ee9312ca47c6d2a655fb7d245d3f79e06777c3
patFish <fischer.pa7rick@gmail.com>
2020-07-15 13:15:57 +0200

    default table

--- src/components/FleetTable.vue
+++ src/components/FleetTable.vue
@@ -4,13 +4,19 @@
       <thead>
         <tr>
           <th class="text-left">Name</th>
-          <th class="text-left">Calories</th>
+          <th class="text-left">VIN</th>
+          <th class="text-left">Make</th>
+          <th class="text-left">Model</th>
+          <th class="text-left">Year</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="item in desserts" :key="item.name">
-          <td>{{ item.name }}</td>
-          <td>{{ item.calories }}</td>
+        <tr v-for="car in cars" :key="car.name">
+          <td>{{ car.name }}</td>
+          <td>{{ car.vin }}</td>
+          <td>{{ car.make }}</td>
+          <td>{{ car.model }}</td>
+          <td>{{ car.year }}</td>
         </tr>
       </tbody>
     </template>
