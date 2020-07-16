07a114a876c13f310c0d51650f8ef330e63bec5b
patFish <fischer.pa7rick@gmail.com>
2020-07-16 16:18:52 +0200

    added proxy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
-}
+  transpileDependencies: ["vuetify"],
+  devServer: {
+    proxy: {
+      "^/": {
+        target: "http://as-loadbalancer-521356964.us-east-2.elb.amazonaws.com",
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
+};
