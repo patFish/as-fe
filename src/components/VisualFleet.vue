e967b11f40fb0fc97dca5b7e1d2ede77645af70a
patFish <fischer.pa7rick@gmail.com>
2020-07-16 09:42:23 +0200

    extracted visual fleet

---
+++ src/components/VisualFleet.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="visualfleet">
+    <Map class="map" :locations="locations" />
+    <FleetTable class="list" :cars="cars" />
+  </div>
+</template>
+
+<script>
+import Map from "./Map";
+import FleetTable from "./FleetTable";
+
+export default {
+  name: "VisualFleet",
+  components: {
+    Map,
+    FleetTable
+  },
+  props: { cars: Array },
+  computed: {
+    locations() {
+      return this.cars.map(car => {
+        return car.location;
+      });
+    }
+  }
+};
+</script>
+
+<style>
+.visualfleet {
+  display: block;
+}
+.map {
+  grid-area: map;
+}
+.list {
+  grid-area: list;
+}
+.header {
+  grid-area: header;
+}
+
+@media only screen and (min-width: 700px) {
+  .visualfleet {
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    grid-template-rows: auto;
+    grid-template-areas:
+      "header header"
+      "map list";
+  }
+}
+</style>
