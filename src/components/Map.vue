429f11ddca4dd1fb2f55d293db17bb04ce4ec953
patFish <fischer.pa7rick@gmail.com>
2020-07-16 18:05:41 +0200

    added default map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -3,7 +3,7 @@
     <l-map
       v-if="showMap"
       :zoom="zoom"
-      :center="locations[0]"
+      :center="locations[0]||defaultCenter"
       :options="mapOptions"
       style="height: 80%"
       @update:center="centerUpdate"
@@ -24,36 +24,11 @@
           </l-popup>
         </l-marker>
       </div>
-      <!-- <l-marker :lat-lng="withPopup">
-        <l-popup>
-          <div @click="innerClick">
-            I am a popup
-            <p v-show="showParagraph">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-              sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi.
-              Donec finibus semper metus id malesuada.
-            </p>
-          </div>
-        </l-popup>
-      </l-marker>
-      <l-marker :lat-lng="withTooltip">
-        <l-tooltip :options="{ permanent: true, interactive: true }">
-          <div @click="innerClick">
-            I am a tooltip
-            <p v-show="showParagraph">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-              sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi.
-              Donec finibus semper metus id malesuada.
-            </p>
-          </div>
-        </l-tooltip>
-      </l-marker>-->
     </l-map>
   </div>
 </template>
 
 <script>
-// import { latLng } from "leaflet";
 import {
   LMap,
   LTileLayer,
@@ -61,8 +36,7 @@ import {
   LPopup
   // , LTooltip
 } from "vue2-leaflet";
-
-import { Icon } from "leaflet";
+import { Icon, latLng } from "leaflet";
 
 import "leaflet/dist/leaflet.css";
 
@@ -72,6 +46,8 @@ Icon.Default.mergeOptions({
   iconUrl: require("leaflet/dist/images/marker-icon.png"),
   shadowUrl: require("leaflet/dist/images/marker-shadow.png")
 });
+
+console.log(process.env);
 
 export default {
   name: "FleetMap",
@@ -92,6 +68,9 @@ export default {
         '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
       currentZoom: 11.5,
       zoom: 10,
+      defaultCenter:
+        latLng(process.env.VUE_MAPCENTER_LAT, process.env.VUE_MAPCENTER_LON) ||
+        latLng(46.8383763, 9.2623928),
       showParagraph: false,
       mapOptions: {
         zoomSnap: 0.5
