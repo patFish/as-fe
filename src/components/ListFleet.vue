9848ac4e386c58579a07a88f4fc4e1359e2a7fab
patFish <fischer.pa7rick@gmail.com>
2020-07-16 09:41:53 +0200

    introduced List View

---
+++ src/components/ListFleet.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="cars"
+    :single-expand="singleExpand"
+    :expanded.sync="expanded"
+    item-key="name"
+    show-expand
+    class="elevation-1 listfleet"
+  >
+    <template v-slot:top></template>
+    <template v-slot:expanded-item="{ headers, item }">
+      <td :colspan="headers.length">
+        <Map :locations="[item.location]" />
+      </td>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+import Map from "./Map";
+export default {
+  components: {
+    Map
+  },
+  props: { cars: Array },
+  data() {
+    return {
+      headers: [
+        {
+          text: "Name",
+          align: "start",
+          value: "name"
+        },
+        { text: "Make", value: "make" },
+        { text: "Model", value: "model" },
+        { text: "Year", value: "year" },
+        { text: "Fuel Type", value: "fuelType" },
+        { text: "Odometer", value: "odometer" },
+        { text: "Type", value: "type" },
+        { text: "VIN", value: "vin" },
+        { text: "Fuel Level", value: "fuel" },
+        { text: "Battery Charge", value: "battery" }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
