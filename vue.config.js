19e731507e8848bc242c039c03f7b0700f7ddbda
patFish <fischer.pa7rick@gmail.com>
2020-07-18 11:24:45 +0200

    set proxy off

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-  devServer: {
-    proxy: {
-      "^/": {
-        target: "http://as-loadbalancer-521356964.us-east-2.elb.amazonaws.com",
-        ws: true,
-        changeOrigin: true,
-      },
-    },
-  },
+  // devServer: {
+  //   proxy: {
+  //     "^/": {
+  //       target: "http://as-loadbalancer-521356964.us-east-2.elb.amazonaws.com",
+  //       ws: true,
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
 };
